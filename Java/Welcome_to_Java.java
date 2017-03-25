cf0c9eabf5851604f5fcaebaec04ff4b8be81c78
Abhishek Verma <abhishek_verma@hotmail.com>
2017-03-26 02:46:31 +0530

    type:  change - white space removed

--- Java/Welcome_to_Java.java
+++ Java/Welcome_to_Java.java
@@ -1,5 +1,4 @@
 public class Solution {
-
     public static void main(String[] args) {
         /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
         System.out.println("Hello, World.");
