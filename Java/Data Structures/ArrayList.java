d419fcfa61317ef384fb280f9635276e0c41b068
Jashanpreet Singh <jsbhullar96@gmail.com>
2017-04-08 20:15:58 +0530

    Type : Some compiler message warning

--- Java/Data Structures/ArrayList.java
+++ Java/Data Structures/ArrayList.java
@@ -1,3 +1,8 @@
+/* Compiler is showing this and if anyone can explain then please Help!
+
+	Note: Solution.java uses unchecked or unsafe operations.
+	Note: Recompile with -Xlint:unchecked for details.
+*/
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.util.StringTokenizer;
