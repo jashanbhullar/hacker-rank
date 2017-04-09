d3b4a582d07fee73401e0e5252321cadc06e226b
Shubham Jaswal <shubhamjaswal772@gmail.com>
2017-04-10 01:25:08 +0530

    SQL

---
+++ SQL/Basic Select/Revising the Select Query II.sql
@@ -0,0 +1,5 @@
+/*
+   Contributer : github.com/jaswal72
+   Email : shubhamjaswal772@gmail.com
+*/
+SELECT NAME FROM CITY WHERE POPULATION > 120000 AND COUNTRYCODE = 'USA';
---
+++ SQL/Basic Select/Select By ID.sql
@@ -0,0 +1,5 @@
+/*
+   Contributer : github.com/jaswal72
+   Email : shubhamjaswal772@gmail.com
+*/
+SELECT * FROM CITY WHERE ID = 1661;
