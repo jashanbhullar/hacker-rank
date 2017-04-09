0ec13a18fb6b737e9369db2aa76f3459790e44f6
Shubham Jaswal <shubhamjaswal772@gmail.com>
2017-04-10 01:24:48 +0530

    SQL

---
+++ SQL/Basic Select/Japanese Cities' Names.sql
@@ -0,0 +1,5 @@
+/*
+   Contributer : github.com/jaswal72
+   Email : shubhamjaswal772@gmail.com
+*/
+SELECT NAME FROM CITY WHERE COUNTRYCODE = 'JPN';
---
+++ SQL/Basic Select/Weather Observation Station 3.sql
@@ -0,0 +1,5 @@
+/*
+   Contributer : github.com/jaswal72
+   Email : shubhamjaswal772@gmail.com
+*/
+SELECT DISTINCT CITY FROM STATION WHERE MOD(ID,2)=0;
