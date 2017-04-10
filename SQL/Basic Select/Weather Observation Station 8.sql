44616249639cba40e4a2b6ba512e9bb87bc50823
Shikhar Gupta <shikharanandg@gmail.com>
2017-04-10 23:49:17 +0530

    SQL

---
+++ SQL/Basic Select/Weather Observation Station 8.sql
@@ -0,0 +1,8 @@
+/*
+* Contributer : github.com/shikhar-07
+* Email : shikharanandg@gmail.com
+*/
+SELECT CITY 
+FROM STATION 
+WHERE (CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' OR CITY LIKE 'U%' ) AND
+      (CITY LIKE '%a' OR CITY LIKE '%e' OR CITY LIKE '%i' OR CITY LIKE '%o' OR CITY LIKE '%u' );
