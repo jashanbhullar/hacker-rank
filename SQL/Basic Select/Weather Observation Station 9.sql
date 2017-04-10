cc841e8a325aa207c47760397a1fb6aa12409f1c
Shikhar Gupta <shikharanandg@gmail.com>
2017-04-10 23:58:26 +0530

    SQL

---
+++ SQL/Basic Select/Weather Observation Station 9.sql
@@ -0,0 +1,8 @@
+/*
+* Contributer : github.com/shikhar-07
+* Email : shikharanandg@gmail.com
+*/
+
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE NOT (CITY LIKE 'A%' OR  CITY  LIKE 'E%' OR CITY  LIKE 'I%' OR CITY  LIKE 'O%' OR CITY  LIKE 'U%');
