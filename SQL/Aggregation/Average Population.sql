bf0d8fe454c28ab36cc8271d5fb66f364a92953c
Shubham Jaswal <shubhamjaswal772@gmail.com>
2017-04-12 02:28:27 +0530

    UPDATE

---
+++ SQL/Aggregation/Average Population.sql
@@ -0,0 +1,5 @@
+/*
+   Contributer : github.com/jaswal72
+   Email : shubhamjaswal772@gmail.com
+*/
+select ROUND(AVG(POPULATION)) FROM CITY;
---
+++ SQL/Aggregation/Revising Aggregations - Averages.sql
@@ -0,0 +1,5 @@
+/*
+   Contributer : github.com/jaswal72
+   Email : shubhamjaswal772@gmail.com
+*/
+SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT ='CALIFORNIA';
