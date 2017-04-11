be563628045dca9f39b07e2e28569d86f195ead3
abhishek_verma@hotmail.com <abhishek_verma@hotmail.com>
2017-04-11 16:12:24 +0530

    Type:Update - New programs for ruby

---
+++ Ruby/Arrays & Hashes/Ruby Array - Addition.rb
@@ -0,0 +1,20 @@
+def end_arr_add(arr, element)
+    # Add `element` to the end of the Array variable `arr` and return `arr`
+    arr.push(element)
+end
+
+def begin_arr_add(arr, element)
+    # Add `element` to the beginning of the Array variable `arr` and return `arr`
+    arr.unshift(element)
+end
+
+def index_arr_add(arr, index, element)
+    # Add `element` at position `index` to the Array variable `arr` and return `arr`
+    arr.insert(index,element)
+end
+
+def index_arr_multiple_add(arr, index)
+    # add any two elements to the arr at the indexins
+    arr.insert(index,12,13)
+
+end
