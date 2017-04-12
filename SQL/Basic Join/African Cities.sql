5d82ce8c991eb5e3b5330254f7c355aa86343d82
Shubham Jaswal <shubhamjaswal772@gmail.com>
2017-04-12 14:46:25 +0530

    SQL UPDATE

---
+++ SQL/Basic Join/African Cities.sql
@@ -0,0 +1,5 @@
+/*
+   Contributer : github.com/jaswal72
+   Email : shubhamjaswal772@gmail.com
+*/
+SELECT CITY.NAME FROM CITY , COUNTRY WHERE CITY.COUNTRYCODE = COUNTRY.CODE AND COUNTRY.CONTINENT='AFRICA';
