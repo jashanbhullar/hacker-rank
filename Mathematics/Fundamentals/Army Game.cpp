25cdd370df573cafa7d41d6d0dac28331c6cfa06
jaswal72 <shubhamjaswal772@gmail.com>
2017-04-21 04:18:49 +0530

    create-army-camp

---
+++ Mathematics/Fundamentals/Army Game.cpp
@@ -0,0 +1,36 @@
+/*
+   Contributer : github.com/jaswal72
+   Email : shubhamjaswal772@gmail.com
+*/
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n,m,s;
+    cin >> n >> m;
+    cout<<((n+1)/2)*((m+1)/2);
+    return 0;
+}
